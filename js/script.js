146b9e64537c2404e49e2e0c96a0b062fd01f8c1
zoerem <63026112+zoerem@users.noreply.github.com>
2020-09-22 16:30:36 -0700

    HW19attemps

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-$("#pC-1").on("mouseenter"{
-    $("#hc-1").css("right" "-80%").css("right" "-40%");
-})
+// $("pC-1").on("mouseenter"(){
+//     $("#hc-1").css("right" "-80%").css("right" "-40%");
+// });
+
+$("pC-1").on("mouseenter"(){
+    $("#hC-1").animate({right: '-40%'});
+});
+
