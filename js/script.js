fa64de278dcf12a71711da244df54b37b4302bca
zoerem <63026112+zoerem@users.noreply.github.com>
2020-09-22 19:49:49 -0700

    arrow animation working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
+$(document).ready(function(){
+
+
+    $("#project-LF").hide();
+
 // $("pC-1").on("mouseenter"(){
 //     $("#hc-1").css("right" "-80%").css("right" "-40%");
 // });
 
-$("pC-1").on("mouseenter"(){
-    $("#hC-1").animate({right: '-40%'});
+// $("#pC-1").on(".mouseenter"(){
+//     $("#project-LF").css("transform", "translateX(-700px)");
+// });
+
+
+
+// $('.arrows-container').on(".mouseenter"(){
+//     $('#arrow-top').effect( "bounce" );
+// });
+
+
+$('.arrows-container').click(function(){
+    // $('#arrow-top').effect( "bounce" );
+    $('#arrow-top').css("animate", "bounceArrow");
 });
 
+    
+// CSS for bounce animation
+// .arrow{
+//     animation: bounceArrow 1s 5;
+// }
+
+
+
+// below closing tag for document-ready function.
+});
